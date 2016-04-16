68f11837936192294dc5a18bbfb7d706ae85cd39
sgoldman <sgoldman216@gmail.com>
2016-04-16 15:41:45 -0600

    Added parts of the build folder, which includes the game engine and viewer jars, the .class files and the automation scripts.

---
+++ Game-Of-Flows/build/classes/RunEngine.sh
@@ -0,0 +1,1 @@
+java -jar FlowsEngine-1.5.jar -detail trace game.out -detail turns turns.out -red java Player -blue java Player
---
+++ Game-Of-Flows/build/classes/RunViewer.sh
@@ -0,0 +1,1 @@
+java -jar FlowsViewer-1.2.jar -trace game.out -scale 60
