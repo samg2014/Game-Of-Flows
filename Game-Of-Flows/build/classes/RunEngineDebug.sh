66c283e0218ce8149be6221abfa872c33d638de2
sgoldman <sgoldman216@gmail.com>
2016-06-01 14:00:57 -0600

    Minor strategy changes here and there. Not much is working very well.

--- Game-Of-Flows/build/classes/RunEngineDebug.sh
+++ Game-Of-Flows/build/classes/RunEngineDebug.sh
@@ -1,1 +1,1 @@
-java -jar FlowsEngine-1.6.jar -detail trace game.out -detail turns turns.out -red java Player -blue java Player
+java -jar FlowsEngine-1.7.jar -detail trace game.out -detail turns turns.out -red java Player -blue java Player
